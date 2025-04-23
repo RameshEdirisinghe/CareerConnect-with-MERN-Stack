47f164c9d694df14b9204d4f0170b163d7bf9168
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2025-04-23 12:56:31 +0530

    function to check if user exists in the db

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -6,6 +6,7 @@ import cors from 'cors';
 import connectDB from './db/connect.js';
 import fs from 'fs';
 import User from './models/userModel.js';
+import asyncHandler from "express-async-handler";
 dotenv.config();
 
 const app = express();
@@ -63,5 +64,30 @@ const server = async () => {
     process.exit(1);
   }
 };
+
+const enusureUserInDB = asyncHandler(async (user) => {
+  try {
+    const existingUser = await User.findOne({ auth0Id: user.sub });
+
+    if (!existingUser) {
+      // create a new user document
+      const newUser = new User({
+        auth0Id: user.sub,
+        email: user.email,
+        name: user.name,
+        role: "jobseeker",
+        profilePicture: user.picture,
+      });
+
+      await newUser.save();
+
+      console.log("User added to db", user);
+    } else {
+      console.log("User already exists in db", existingUser);
+    }
+  } catch (error) {
+    console.log("Error checking or adding user to db", error.message);
+  }
+});
 
 server();
