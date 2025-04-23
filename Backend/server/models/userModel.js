2d95930d6e462c96262b58ebd4b424f95e41d1b8
Ramesh Edirisinghe <e2145434@bit.uom.lk>
2025-04-23 12:13:04 +0530

    update backend server

--- Backend/server/models/userModel.js
+++ Backend/server/models/userModel.js
@@ -1,46 +1,62 @@
-import { auth } from "express-openid-connect";
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    auth0Id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    appliedJobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+
+    savedJobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+
+    role: {
+      type: String,
+      enum: ["jobseeker", "recruiter"],
+      default: "jobseeker",
+    },
+
+    resume: {
+      type: String,
+    },
+
+    profilePicture: {
+      type: String,
+    },
+
+    bio: {
+      type: String,
+      default: "No bio provided",
+    },
+
+    profession: {
+      type: String,
+      default: "Unemployed",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  auth_id: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  appliedJobs: {
-    type:mongoose.Schema.Types.ObjectId,
-    ref: 'Job',
-  },
-  savedJobs: {
-    type:mongoose.Schema.Types.ObjectId,
-    ref: 'Job',
-  },
-  role:{
-    type: String,
-    enum: ['jobseeker', 'recruiter'],
-    default: 'jobseeker'
-  },
-  profilePic: {
-    type: String,
-  },
-  bio: {
-    type: String,
-    default: 'No bio provided'
-  },
-  profession: {
-    type: String,
-    default: 'Unemployed'
-  },
-},
   { timestamps: true }
 );
 
